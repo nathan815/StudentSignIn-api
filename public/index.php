1b4318c74161ae7536396674a7c34205ed8ec1a6
Nathaniel Johnson <nathancj224@gmail.com>
2018-02-02 01:18:10 -0500

    Basic slim route

--- public/index.php
+++ public/index.php
@@ -1,6 +1,14 @@
 <?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 use \Slim\App;
 
 $app = new \Slim\App();
+$app->get('/hello/{name}', function (Request $req,  Response $res, $args = []) {
+    $name = $args['name'];
+    $res->getBody()->write("Hello, $name");
+    return $res;
+});
+$app->run();
